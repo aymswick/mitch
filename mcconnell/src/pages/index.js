4e0e4d0ef56a7eb0b611f66b1a0fd0d90a822ad2
Anthony Symkowick <>
2020-09-18 18:04:48 -0700

    Initial commit.

---
+++ mcconnell/src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "../styles/global.css"
+
+export default function Home() {
+  return (
+    <div class="root">
+      <h1>No :(</h1>
+      <h4>Please check back frequently.</h4>
+    </div>
+  )
+}
