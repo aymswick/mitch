7a474e763cf3890f5ecdd1cbaa978bb279d8e09e
Anthony Symkowick <9097666+aymswick@users.noreply.github.com>
2023-03-10 21:16:36 -0600

    Update index.js

--- mcconnell/src/pages/index.js
+++ mcconnell/src/pages/index.js
@@ -4,7 +4,8 @@ import "../styles/global.css"
 export default function Home() {
   return (
     <div class="root">
-      <h1>No :(</h1>
+      <h1>UPDATE!!!<h1>
+      <h2>Not yet, but maybe soon.</h2>
       <h4>Please check back frequently.</h4>
     </div>
   )
